f0c3c87860dae521729f3f28661d0075372905b4
rafleeea <rafli.10522118@mahasiswa.unikom.ac.id>
2025-10-04 04:58:04 +0700

    update

--- src/data/Projects.jsx
+++ src/data/Projects.jsx
@@ -1,13 +1,13 @@
 import LMS from "../assets/lms.png";
 import SweetDessert from "../assets/sweetdessert.png";
-import Xiujank from "../assets/xiujank.png"
-import Tribe from "../assets/sweetdessert.png"
-import Petadik from "../assets/petadik.jpg"
+import Xiujank from "../assets/xiujank.png";
+import Tribe from "../assets/progress.png";
+import Petadik from "../assets/petadik.jpg";
 
 const Projects = [
   {
     title: "PETADIK",
-    date: "Jul 2025 - Aug 2025",
+    date: "Aug 2025 - Sep 2025",
     description:
       "Focused on creating a feature for periodic salary increases for teachers and designing an interactive dashboard using Bootstrap.",
     image: Petadik,
@@ -26,7 +26,7 @@ const Projects = [
   },
   {
     title: "Learning Management System",
-    date: "Feb 2025 - Jun 2025",
+    date: "Feb 2025 - Jul 2025",
     description:
       "Contributed to the development of the company's internal LMS website UI using HTML, CSS, JavaScript, Bootstrap, and Moodle customization.",
     image: LMS,
@@ -40,17 +40,15 @@ const Projects = [
       "Designed and developed an e-commerce website, handling both frontend and backend development using Flask (Python), MongoDB, and Bootstrap.",
     image: SweetDessert,
     techStack: ["Flask", "MongoDB", "Bootstrap"],
-    website: "https://sweetdessert.glitch.me/",
     github: "https://github.com/hadimughny7/SweetDessertWeb",
   },
   {
     title: "tribe",
-    date: "Jun 2025 - Present",
+    date: "Jul 2025 - Present",
     description:
       "Designing and developing a streetwear fashion e-commerce platform with React, Vite, MongoDB, TailwindCSS, and Redis for caching.",
-    image: "",
+    image:[Tribe],
     techStack: ["React", "Express", "TailwindCSS", "Redis", "MongoDB"],
-    website: "test",
     github: "https://github.com/RafliAAA/tribe/",
   },
 ];
